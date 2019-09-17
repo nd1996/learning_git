092cea110539747933cbe80455bf5d946b43e26a
git user 2 <nileshdubeynd5101@gmail.com>
2019-09-17 17:01:07 +0530

    Base file is not seem to be proper

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<title lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Hello Nilesh by user 2</title>
+  <title>Hello Nilesh by u</title>
 </head>
 <body>
     <?php echo "Git User 2"; ?>
     <h1>Hello World</h1>
-    <p> helloworl </p>
 </body>
 </html>
